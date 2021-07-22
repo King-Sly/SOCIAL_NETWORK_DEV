e37a6f598e8df9467445da07e6284e0a1a2a36ce
Kingsley Okeke <nkokeke@ualberta.ca>
2021-07-22 14:37:38 -0600

    Front-End loading

--- server.js
+++ server.js
@@ -5,9 +5,18 @@ const app = express();
 //CONNECT THE DATABASE====(I'VE WRITTEN THIS FOR LIKE 1000 times in the past four months)
 connectDB();
 
+//Middleware
+app.use(express.json({extended : false}));
+// app.use(express.json({extended : false}));
 const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => res.send('API Running'));
+
+//DEFINE ROUTES====SAME WITH THIS(998 times)
+app.use('/api/users', require("./routes/api/users"));
+app.use('/api/auth', require("./routes/api/auth"));
+app.use('/api/profile', require("./routes/api/profile"));
+app.use('/api/posts', require("./routes/api/posts"));
 
 app.listen(PORT, () => {
     console.log(`Server started on ${PORT}`);

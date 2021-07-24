42a9c2f32e07441e9254aad43e9cd4d4a7f41030
Kingsley Okeke <nkokeke@ualberta.ca>
2021-07-24 11:47:22 -0600

    Github Api Interaction=success

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const router = express.Router();
+const request = require('request');
+const config = require('config');
 const auth = require("../../middleware/auth");
 const Profile = require('../../models/Profile');
 const User = require("../../models/Profile");
 const {check, validationResult} = require('express-validator');
+const { response } = require('express');
 
 //we can always access user.id which is same as user._id
 // @route   GET api/profile/me
@@ -279,4 +282,28 @@ router.delete('/education/:edu_id', auth, async (req, res) => {
     }
 });
 
+
+// @route   GET api/profile/github/:username
+// @desc    Get user repos from Github
+// @access  Public
+router.get("/github/:username", (req, res) => {
+    try {
+        const options = {
+            uri :  `https://api.github.com/users/${req.params.username}/repos?per_page=5&sort=created:asc&client_id=${config.get('githubClientId')}&client_secret=${config.get('githubSecret')}`, 
+            method : 'GET', 
+            headers : {"user-agent" : "node.js"}
+        };
+        request(options, (err, response, body) => {
+            if(err) console.error(err);
+            if(response.statusCode !== 200) {
+                return res.status(404).json({msg : "No Github profile found"});
+            }
+            res.json(JSON.parse(body));
+        })
+    }
+    catch(err) {
+        console.error(err.message);
+        res.status(500).send("Server Error");
+    }
+})
 module.exports = router;

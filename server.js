caa69aab98def43980cf2258c7d7a53a769dfa28
Kingsley Okeke <nkokeke@ualberta.ca>
2021-08-14 21:52:01 -0600

    Preparing to deploy for now..further updates incoming

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const connectDB = require("./config/db");
+const path = require('path');
 
 const app = express();
 //CONNECT THE DATABASE====(I'VE WRITTEN THIS FOR LIKE 1000 times in the past four months)
@@ -8,9 +9,8 @@ connectDB();
 //Middleware
 app.use(express.json({extended : false}));
 // app.use(express.json({extended : false}));
-const PORT = process.env.PORT || 5000;
 
-app.get('/', (req, res) => res.send('API Running'));
+// app.get('/', (req, res) => res.send('API Running'));
 
 //DEFINE ROUTES====SAME WITH THIS(998 times)
 app.use('/api/users', require("./routes/api/users"));
@@ -18,6 +18,19 @@ app.use('/api/auth', require("./routes/api/auth"));
 app.use('/api/profile', require("./routes/api/profile"));
 app.use('/api/posts', require("./routes/api/posts"));
 
+
+//Serve static assets in production 
+if(process.env.NODE_ENV == 'production'){
+    //Set static folder
+    app.use(express.static('client/build'));
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+
+}
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
     console.log(`Server started on ${PORT}`);
 });
+
+
